57e105051bdcf9a0ae783b7d443c39f51ff4ffec
lishuo3331 <lishuo20162012>
2020-04-14 18:15:19 +0800

    攻击二分类
    攻击二分类

--- model/dump_best_rf.py
+++ model/dump_best_rf.py
@@ -11,8 +11,32 @@ print("data loaded")
 y = df["attack_type"]
 X = df[selected_feat_names]
 
+
 rfc = RandomForestClassifier(n_jobs=-1, n_estimators=35, criterion="entropy")
 rfc.fit(X, y)
+
+'''
+
+'''
+estimator = rfc.estimators_[1]
+
+from sklearn.tree import export_graphviz
+# 导出为dot 文件
+export_graphviz(estimator, out_file='tree.dot',
+                feature_names = selected_feat_names,
+                class_names = "attack_type",
+                rounded = True, proportion = False,
+                precision = 2, filled = True)
+
+# 用系统命令转为PNG文件(需要 Graphviz)
+from subprocess import call
+call(['dot', '-Tpng', 'tree.dot', '-o', 'tree.png', '-Gdpi=100'])
+
+# 在jupyter notebook中展示
+from IPython.display import Image
+Image(filename = 'tree.png')
+
+
 print("training finished")
 
 # save model for later use
--- prediction/make_predictions.py
+++ prediction/make_predictions.py
@@ -16,18 +16,67 @@ print("data loaded")
 # rf 打分
 with open('../data/rf.pkl', 'rb') as f:
     rf = pickle.load(f)
-y_rf = rf.predict(X)   #攻击类型猜测值
-print("rf results:")
-index = 0;
-wrong_number= 0
-for a in y:
-    if(y[index] == y_rf[index]):
-        index +=1
-    else:
-        print('index: ',index,'true value: ',y[index],' pre value: ',y_rf[index])
-        wrong_number +=1
-        index +=1
-print('%.4f%%'%((1-wrong_number/index)*100))
+
+#ROC line
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import svm, datasets
+from sklearn.metrics import roc_curve, auc  ###计算roc和auc
+from sklearn import model_selection
+
+# Import some data to play with
+
+
+##变为2分类
+# X, y = X[y != 2], y[y != 2]
+
+# Add noisy features to make the problem harder
+random_state = np.random.RandomState(0)
+n_samples, n_features = X.shape
+# X = np.c_[X, random_state.randn(n_samples, 200 * n_features)]
+
+# shuffle and split training and test sets
+# X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=.3,random_state=0)
+
+# Learn to predict each class against the other
+# svm = svm.SVC(kernel='linear', probability=True,random_state=random_state)
+
+###通过decision_function()计算得到的y_score的值，用在roc_curve()函数中
+y_score = rf.predict(X)
+
+# Compute ROC curve and ROC area for each class
+fpr,tpr,threshold = roc_curve(y, y_score) ###计算真正率和假正率
+roc_auc = auc(fpr,tpr) ###计算auc的值
+
+plt.figure()
+lw = 2
+plt.figure(figsize=(10,10))
+plt.plot(fpr, tpr, color='darkorange',
+         lw=lw, label='ROC curve (area = %0.2f)' % roc_auc) ###假正率为横坐标，真正率为纵坐标做曲线
+plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('rf_kdd99')
+plt.legend(loc="lower right")
+plt.show()
+
+
+
+
+# y_rf = rf.predict(X)   #攻击类型猜测值
+# print("rf results:")
+# index = 0;
+# wrong_number= 0
+# for a in y:
+#     if(y[index] == y_rf[index]):
+#         index +=1
+#     else:
+#         print('index: ',index,'true value: ',y[index],' pre value: ',y_rf[index])
+#         wrong_number +=1
+#         index +=1
+# print('%.4f%%'%((1-wrong_number/index)*100))
 # np.set_printoptions(threshold=np.inf)#输出全部元素
 # print(y_rf)
 
